a44a90dd8ae7fbe7ee198d340f4541a8030c67e9
Bruno Paulino <bruno@bpaulino.com>
2024-06-26 21:58:37 +0200

    Add module docs

--- lib/fire_sale_web/presence.ex
+++ lib/fire_sale_web/presence.ex
@@ -1,4 +1,7 @@
 defmodule FireSaleWeb.Presence do
+  @moduledoc """
+  Track presence across the FireSale App
+  """
   use Phoenix.Presence,
     otp_app: :fire_sale,
     pubsub_server: FireSale.PubSub
